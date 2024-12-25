775d0eafb289bde64acd6d5a98849683b3981592
Ayush-3012 <diveshsrivastava31@gmail.com>
2024-12-25 22:28:31 +0530

    api deployment #4

--- src/index.js
+++ src/index.js
@@ -21,6 +21,10 @@ app.get("/", (req, res) => res.send("Hello, Welcome!"));
 //   })
 //   .catch((err) => console.log("MongoDb Connection failed: ", err));
 
+app.listen(process.env.PORT, () => {
+  console.log(` Server is listening to port ${process.env.PORT}`);
+});
+
 // import loveBabbarRouter from "./routes/loveBabbar.routes.js";
 // import shradhaDiRouter from "./routes/shradhaDi.routes.js";
 
