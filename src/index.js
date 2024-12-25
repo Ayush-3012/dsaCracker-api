3c6fe9d12b426e181ae5f222a9277d0f0eb89e19
Ayush-3012 <diveshsrivastava31@gmail.com>
2024-12-25 22:41:32 +0530

    api deployment #9

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
-import connectDB from "./config/connect.js";
+import { connectDB } from "./config/connect.js";
 import cors from "cors";
 import { seedData } from "./seed.js";
 dotenv.config();
@@ -14,15 +14,15 @@ app.get("/", (req, res) => res.send("Hello, Welcome!"));
 
 connectDB()
   .then(() => {
-    // seedData();
+    seedData();
     app.listen(process.env.PORT, () => {
       console.log(` Server is listening to port ${process.env.PORT}`);
     });
   })
   .catch((err) => console.log("MongoDb Connection failed: ", err));
 
-// import loveBabbarRouter from "./routes/loveBabbar.routes.js";
-// import shradhaDiRouter from "./routes/shradhaDi.routes.js";
+import loveBabbarRouter from "./routes/loveBabbar.routes.js";
+import shradhaDiRouter from "./routes/shradhaDi.routes.js";
 
-// app.use("/api/v1/loveBabbarQuestions", loveBabbarRouter);
-// app.use("/api/v1/shrdhaDiQuestions", shradhaDiRouter);
+app.use("/api/v1/loveBabbarQuestions", loveBabbarRouter);
+app.use("/api/v1/shrdhaDiQuestions", shradhaDiRouter);
