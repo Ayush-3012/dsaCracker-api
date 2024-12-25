b458d7f53c1c9b6679f5e119547f81d43630c7b3
Ayush-3012 <diveshsrivastava31@gmail.com>
2024-12-25 22:35:55 +0530

    api deployment #8

--- src/index.js
+++ src/index.js
@@ -12,18 +12,14 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("Hello, Welcome!"));
 
-app.listen(process.env.PORT, () => {
-  console.log(` Server is listening to port ${process.env.PORT}`);
-});
-
-connectDB();
-//   .then(() => {
-//     // seedData();
-//     app.listen(process.env.PORT, () => {
-//       console.log(` Server is listening to port ${process.env.PORT}`);
-//     });
-//   })
-//   .catch((err) => console.log("MongoDb Connection failed: ", err));
+connectDB()
+  .then(() => {
+    // seedData();
+    app.listen(process.env.PORT, () => {
+      console.log(` Server is listening to port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("MongoDb Connection failed: ", err));
 
 // import loveBabbarRouter from "./routes/loveBabbar.routes.js";
 // import shradhaDiRouter from "./routes/shradhaDi.routes.js";
